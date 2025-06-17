a37e16e2ac8512d59215a77afc543a24652309cc
Christian Salinas <cmsalinas2000@gmail.com>
2025-06-17 12:51:35 -0500

    made facny color haha

--- crystalcards/src/components/navbar.tsx
+++ crystalcards/src/components/navbar.tsx
@@ -1,22 +1,23 @@
 import Link from 'next/link';
 import {usePathname} from 'next/navigation';
+import styles from '../styles/Navbar.module.css';
 export default function Navbar(){
 
     const pathname = usePathname();
     return (
-        <nav className="navbar">
-            <ul className="navList">
-                <li className="navItem">
-                    <Link href="/" className={`navLink ${pathname === '/' ? 'active' : ''}`}>Home</Link>
+        <nav className={styles.navbar}>
+            <ul className={styles.navList}>
+                <li className={styles.navItem}>
+                    <Link href="/" className={`${pathname === '/' ? 'active' : ''}`}>Home</Link>
                 </li>
-                <li className="navItem">
-                    <Link href="/decks" className={`navLink ${pathname === '/decks' ? 'active' : ''}`}>Decks</Link>
+                <li className={styles.navItem}>
+                    <Link href="/decks" className={`${pathname === '/decks' ? 'active' : ''}`}>Decks</Link>
                 </li>
-                <li className="navItem">
-                    <Link href="/cards" className={`navLink ${pathname === '/cards' ? 'active' : ''}`}>Cards</Link>
+                <li className={styles.navItem}>
+                    <Link href="/cards" className={`${pathname === '/cards' ? 'active' : ''}`}>Cards</Link>
                 </li>
-                <li className="navItem">
-                    <Link href="/about" className={`navLink ${pathname === '/about' ? 'active' : ''}`}>About</Link>
+                <li className={styles.navItem}>
+                    <Link href="/about" className={`${pathname === '/about' ? 'active' : ''}`}>About</Link>
                 </li>
             </ul>
         </nav>
